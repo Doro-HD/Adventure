710800ad73c988b4d45da10547dbcf199a0c192b
david <david.bjakobsen@gmail.com>
2021-10-01 11:51:09 +0200

    added my solution to the Room class

--- src/Adventure.java
+++ src/Adventure.java
@@ -1,2 +1,19 @@
 public class Adventure {
+    public static void main(String[] args) {
+        /*Room room1 = new Room("test1");
+        Room room2 = new Room("test2");
+        Room room3 = new Room("test3");
+        Room room4 = new Room("test4");
+        Room room5 = new Room("test5");
+        Room room6 = new Room("test6");
+        Room room7 = new Room("test7");
+        Room room8 = new Room("test8");
+        Room room9 = new Room("test9");
+
+        //sets the exits to form a 3x3 square grid.
+        room2.setHorizontalExits(room1, room3); // room 2 westExit er room1 og dens eastExit er room3
+        room4.setVerticalExits(room1, room7);
+        room6.setHorizontalExits(room3, room9);
+        room8.setThreeWay(room5, room7, room9);*/
+    }
 }
--- src/Room.java
+++ src/Room.java
@@ -1,2 +1,89 @@
 public class Room {
+    /*private String name;
+
+    private Room northExit;
+    private Room southExit;
+    private Room eastExit;
+    private Room westExit;
+
+    public Room(String name) {
+        this.setName(name);
+    }
+
+    public void printExits() {
+        System.out.println("North exit is: " + this.northExit);
+        System.out.println("South exit is: " + this.southExit);
+        System.out.println("West exit is: " + this.westExit);
+        System.out.println("East exit is: " + this.eastExit);
+    }
+
+    public String toString() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNorthExit(Room room) {
+        this.northExit = room;
+
+        if (room.southExit == null) {
+            room.setSouthExit(this);
+        }
+    }
+
+    public Room getNorthExit() {
+        return this.northExit;
+    }
+
+    public void setSouthExit(Room room) {
+        this.southExit = room;
+
+        room.setNorthExit(this);
+    }
+
+    public Room getSouthExit() {
+        return this.southExit;
+    }
+
+    public void setWestExit(Room room) {
+        this.westExit = room;
+
+        if (room.eastExit == null) {
+            room.setEastExit(this);
+        }
+    }
+
+    public Room getWestExit() {
+        return this.westExit;
+    }
+
+    public void setEastExit(Room room) {
+        this.eastExit = room;
+
+        if (room.westExit == null) {
+            room.setWestExit(this);
+        }
+    }
+
+    public Room getEastExit() {
+        return this.eastExit;
+    }
+
+    public void setVerticalExits(Room northExit, Room southExit) {
+        this.setNorthExit(northExit);
+        this.setSouthExit(southExit);
+    }
+
+    public void setHorizontalExits(Room westExit, Room eastExit) {
+        this.setEastExit(eastExit);
+        this.setWestExit(westExit);
+    }
+
+    public void setThreeWay(Room northExit, Room westExit, Room eastExit) {
+        this.setNorthExit(northExit);
+        this.setWestExit(westExit);
+        this.setEastExit(eastExit);
+    }*/
 }
