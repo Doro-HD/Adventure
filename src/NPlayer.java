4b9ac39b86971ca57c9f52bf0de2fb336a0c954e
nann6665 <nann6665@kea.stud.dk>
2021-10-04 11:28:55 +0200

    player class

---
+++ src/NPlayer.java
@@ -0,0 +1,16 @@
+public class NPlayer {
+    private String playerName;
+    private Room playerLocation;
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+    public String getPlayerName() {
+        return this.playerName;
+    }
+
+    public void setplayerLocation(Room newPlayerLocation) {
+        this.playerLocation = newPlayerLocation;
+    }
+
+}
