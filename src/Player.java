03e722bc8d9091f3573d74aee0f8de0996b1e5b5
david <david.bjakobsen@gmail.com>
2021-10-22 11:58:02 +0200

    Command.java: Made some changes to when the direction that the player is going in is printed out onto the screen.
    Player.java:
    Added methods for taking and dropping items from/in rooms.

--- src/Player.java
+++ src/Player.java
@@ -1,10 +1,36 @@
+import java.util.ArrayList;
+
 public class Player {
     private String name;
+    private ArrayList<Item> inventory;
     private Room currentRoom;
 
     public Player(String name, Room startPosition) {
         this.setName(name);
         this.setCurrentRoom(startPosition, false);
+        this.inventory = new ArrayList<>();
+    }
+
+    //todo: make sure this method supports the way that the item is written by Nana
+    public void takeItem(String itemName) {
+        for (int i = 0; i < this.currentRoom.getItems().size(); i++) {
+            if (this.currentRoom.getItems().get(i).getName().equals(itemName)) {
+                this.inventory.add(this.currentRoom.getItems().get(i));
+                this.currentRoom.getItems().remove(i);
+                i++;
+            }
+        }
+    }
+
+    //todo: make sure this method supports the way that the item is written by Nana
+    public void dropItem(String itemName) {
+        for (int i = 0; i < this.inventory.size(); i++) {
+            if (this.inventory.get(i).getName().equals(itemName)) {
+                this.currentRoom.getItems().add(this.inventory.get(i));
+                this.inventory.remove(i);
+                i++;
+            }
+        }
     }
 
     public void setName(String name) {
@@ -15,10 +41,15 @@ public class Player {
         return this.name;
     }
 
+    public void setCurrentRoom(Room newPlayerPosition, boolean printLocation) {
+        this.setCurrentRoom(newPlayerPosition, "", printLocation);
+    }
+
     //Sets the players current position to be a new room unless that room is null
     //The argument given in the client code is a reference to another room
-    public void setCurrentRoom(Room newPlayerPosition, boolean printLocation) {
+    public void setCurrentRoom(Room newPlayerPosition, String direction, boolean printLocation) {
         if (newPlayerPosition != null) {
+            System.out.println("Going " + direction);
             this.currentRoom = newPlayerPosition;
             if (printLocation) {
                 this.printLocation();
