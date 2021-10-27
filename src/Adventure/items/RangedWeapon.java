8181cc7a4f2641cecafe4cff5804211c0cb0521d
Mojens <moha135h@stud.kea.dk>
2021-10-27 12:35:39 +0200

    Tilføjet ny attribut til RangedWeapon.java

--- src/Adventure/items/RangedWeapon.java
+++ src/Adventure/items/RangedWeapon.java
@@ -1,7 +1,9 @@
 package Adventure.items;
 
 public class RangedWeapon extends Weapon {
-  public RangedWeapon(String type, String name, String description, int damage){
+  private int ammunition;
+  public RangedWeapon(String type, String name, String description, int damage,int ammunition){
     super(name, description, type, damage);
+    this.ammunition = ammunition;
   }
 }
