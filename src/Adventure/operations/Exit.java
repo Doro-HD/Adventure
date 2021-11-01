c596b356728e0da5ed5bf151b95021e1e6eae3d8
david <david.bjakobsen@gmail.com>
2021-11-01 14:54:13 +0100

    Added ability to exit via a command although you cant exit the game right now

---
+++ src/Adventure/operations/Exit.java
@@ -0,0 +1,11 @@
+package Adventure.operations;
+
+import Adventure.core.Player;
+
+public class Exit extends Operation {
+
+    public boolean execute(Player player) {
+        return false;
+    }
+
+}
