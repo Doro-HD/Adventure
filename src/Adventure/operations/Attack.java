f8233f5b9b67a417e96898a6c426754fde011aab
david <david.bjakobsen@gmail.com>
2021-11-01 15:29:04 +0100

    Added more cases to the Attack operation

--- src/Adventure/operations/Attack.java
+++ src/Adventure/operations/Attack.java
@@ -15,6 +15,15 @@ public class Attack extends Operation{
     Status status = player.checkAttack();
 
     switch (status) {
+      case enemyNotFound:
+        this.operationExecution += "There is no enemy in the room";
+        break;
+      case itemNotFound:
+        this.operationExecution += "You must equip a weapon before trying to attack an enemy";
+        break;
+      case notUsable:
+        this.operationExecution += "You have no ammo left for your weapon";
+        break;
       case enemyIsDead:
         this.operationExecution += "Congratulations you killed the enemy.";
         break;
